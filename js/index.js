016b5713d91278fb83255e4ba63c634b2d051895
Sebastián Araya <arayariquelmes@gmail.com>
2020-10-21 15:25:31 -0300

    Generar el molde de lo cards por personaje
    Generar el molde en html
    Generar la funcion javascript que copia el molde por cada
    personaje
    Vincular el personaje al boton

--- js/index.js
+++ js/index.js
@@ -1,21 +1,25 @@
+window.mostrarPersonaje = function(){
+    //Obtener el personaje
+    let p = this.personaje;
+    console.log(p);
+    //TODO: 1.Crear un Sweet Alert
+    //TODO: 2. Agregar el personaje a ese SweetAlert
+    //TODO: 3. Agregar iconos diferentes para cuando esta vivo, muerto o desaparecido
+};
 window.mostrar = (datos)=>{
     const contenidoDiv = document.querySelector("#contenido");
-    let ul = document.createElement("ul");
+    let molde = document.querySelector('.card-molde');
     for(let i=0; i < datos.length; ++i){
         let personaje = datos[i];
-        let li = document.createElement("li");
-        let spanNombre = document.createElement("span");
-        spanNombre.innerText = personaje.name;
-        let spanEstado = document.createElement("span");
-        spanEstado.innerText = personaje.status;
-        let imagen = document.createElement("img");
-        imagen.src = personaje.image;
-        li.appendChild(spanNombre);
-        li.appendChild(spanEstado);
-        li.appendChild(imagen);
-        ul.appendChild(li);
+        let div = molde.cloneNode(true);
+        div.querySelector('.nombre-personaje').innerText = personaje.name;
+        div.querySelector(".imagen-personaje").src = personaje.image;
+        div.querySelector('.btn-detalle-personaje').personaje = personaje;
+        div.querySelector('.btn-detalle-personaje')
+            .addEventListener('click',window.mostrarPersonaje);
+        contenidoDiv.appendChild(div);
     }
-    contenidoDiv.appendChild(ul);
+    
 };
 window.addEventListener('DOMContentLoaded', async ()=>{ //Se ejecuta cuando se carga la página
     //Todo lo que ponga aca dentro se va a ejecutar cuando la pagina este totalmente cargada.
