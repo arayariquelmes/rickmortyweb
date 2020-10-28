a1d98f9cd52177a074c838d6a7cb2535c0e8e65b
Sebastián Araya <arayariquelmes@gmail.com>
2020-10-28 16:07:58 -0300

    Agregar ventana modal de rick y morty
    Agregar modal con sweet alert
    Agregar iconos con fontawesome

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
 window.mostrarPersonaje = function(){
     //Obtener el personaje
     let p = this.personaje;
-    console.log(p);
-    //TODO: 1.Crear un Sweet Alert
+    let molde = document.querySelector('.modal-molde').cloneNode(true);
+    molde.querySelector('.imagen-personaje-modal').src = p.image;
+    molde.querySelector('.nombre-personaje-h3').innerText = p.name;
+    molde.querySelector('.genero-personaje-h3').innerText = p.gender;
+    let spanEstado = molde.querySelector('.estado-personaje-sp');
+    let icono = document.createElement('i');
+    icono.classList.add('icono-estado');
+    if(p.status =='Alive'){
+        icono.classList.add('fas','fa-heart', 'text-info');
+    }else if(p.status == 'Dead'){
+        icono.classList.add('fas', 'fa-book-dead', 'text-danger');
+    } else {
+        icono.classList.add('fas','fa-question', 'text-warning');
+    }
+    //Agregar el icono al molde
+    spanEstado.appendChild(icono);
+    Swal.fire({
+        title: p.name,
+        html: molde.innerHTML,
+        confirmButtonText: "Cerrar"
+    });
     //TODO: 2. Agregar el personaje a ese SweetAlert
     //TODO: 3. Agregar iconos diferentes para cuando esta vivo, muerto o desaparecido
 };
